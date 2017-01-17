0cbbf636fd22259fc2884cdfe2b39fe4dbd9df22
unknown <inanielhoussain@gmail.com>
2017-01-17 10:18:41 +0000

    set up package

---
+++ src/CodebirdWrapperServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+namespace Inani\CodebirdWrapper;
+
+use Illuminate\Support\ServiceProvider;
+
+class CodebirdWrapperServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings
+     *
+     */
+    public function register()
+    {
+/*        $this->app->bind('TwitterWrapper', function() {
+
+        });*/
+    }
+}
